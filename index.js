d06a0ee485141079362f61c3d908edc3bb096399
Rubaid07 <rubaidindu@gmail.com>
2025-06-11 20:11:56 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -2,11 +2,44 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json"); 
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+)
 app.use(express.json())
+
+const verifyToken = async (req, res, next) => {
+  const authorizationHeader = req?.headers?.authorization;
+  if (!authorizationHeader) {
+    return res.status(401).send({ message: 'Unauthorized Access: No token provided!' });
+  }
+
+  const token = authorizationHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized Access: Invalid token format!' });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.decodedEmail = decodedToken.email;
+    next();
+  } catch (err) {
+    console.error("Error verifying Firebase ID token:", err);
+    return res.status(403).send({ message: 'Forbidden Access: Invalid or expired token!' });
+  }
+};
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynxyt70.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -24,9 +57,13 @@ async function run() {
   try {
     const db = client.db("pack2goDB");
     const packageCollection = db.collection("tourPackages");
+    const bookingCollection = db.collection("bookings");
 
-    app.post('/packages', async (req, res) => {
+    app.post('/packages', verifyToken, async (req, res) => {
       const newPackage = req.body;
+      if (req.decodedEmail !== newPackage.guide_email) {
+        return res.status(403).send({ message: 'Forbidden Access: Email mismatch!' });
+      }
       const result = await packageCollection.insertOne(newPackage);
       res.send(result);
     });
@@ -47,55 +84,80 @@ async function run() {
       res.send(result);
     });
 
-    app.post('/bookings', async (req, res) => {
+    app.post('/bookings', verifyToken, async (req, res) => {
       const booking = req.body;
-      const result = await db.collection('bookings').insertOne(booking);
+      if (req.decodedEmail !== booking.buyer_email) {
+        return res.status(403).send({ message: 'Forbidden Access: Email mismatch!' });
+      }
+      const result = await bookingCollection.insertOne(booking); 
 
-      await db.collection('tourPackages').updateOne(
+      await packageCollection.updateOne(
         { _id: new ObjectId(booking.tour_id) },
         { $inc: { bookingCount: 1 } }
       );
       res.send(result);
     });
 
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', verifyToken, async (req, res) => {
       const email = req.query.email;
-      const bookings = await db.collection('bookings').find({ buyer_email: email }).toArray();
+      const tokenEmail = req.decodedEmail; 
+      if (email !== tokenEmail) {
+        return res.status(403).send({ message: 'Forbidden Access: Email mismatch!' });
+      }
+      const bookings = await bookingCollection.find({ buyer_email: email }).toArray();
       res.send(bookings);
     });
 
-    app.patch('/bookings/:id', async (req, res) => {
+    app.patch('/bookings/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
-      const result = await db.collection('bookings').updateOne(
+      const bookingToUpdate = await bookingCollection.findOne({ _id: new ObjectId(id) });
+      if (!bookingToUpdate || bookingToUpdate.guide_email !== req.decodedEmail) {
+        return res.status(403).send({ message: 'Forbidden Access: Not authorized to confirm this booking!' });
+      }
+
+      const result = await bookingCollection.updateOne(
         { _id: new ObjectId(id) },
         { $set: { status: "completed" } }
       );
       res.send(result);
     });
 
-    app.get('/my-packages', async (req, res) => {
+    app.get('/my-packages', verifyToken, async (req, res) => {
       const email = req.query.email;
-      const myPackages = await db.collection('tourPackages').find({ guide_email: email }).toArray();
+      const tokenEmail = req.decodedEmail; 
+      if (email !== tokenEmail) {
+        return res.status(403).send({ message: 'Forbidden Access: Email mismatch!' });
+      }
+      const myPackages = await packageCollection.find({ guide_email: email }).toArray();
       res.send(myPackages);
     });
 
-    app.put('/packages/:id', async (req, res) => {
+    app.put('/packages/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
       const updated = req.body;
-      const result = await db.collection('tourPackages').updateOne(
-        { _id: new ObjectId(req.params.id) },
+      const existingPackage = await packageCollection.findOne({ _id: new ObjectId(id) });
+      if (!existingPackage || existingPackage.guide_email !== req.decodedEmail) {
+        return res.status(403).send({ message: 'Forbidden Access: Not authorized to update this package!' });
+      }
+      const result = await packageCollection.updateOne(
+        { _id: new ObjectId(id) },
         { $set: updated }
       );
       res.send(result);
     });
 
-    app.delete('/packages/:id', async (req, res) => {
-      const result = await db.collection('tourPackages').deleteOne({ _id: new ObjectId(req.params.id) });
+    app.delete('/packages/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const existingPackage = await packageCollection.findOne({ _id: new ObjectId(id) });
+      if (!existingPackage || existingPackage.guide_email !== req.decodedEmail) {
+        return res.status(403).send({ message: 'Forbidden Access: Not authorized to delete this package!' });
+      }
+      const result = await packageCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
 
   } finally {
-
   }
 }
 run().catch(console.dir);
